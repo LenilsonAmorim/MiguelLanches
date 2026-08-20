913bf4334a2c69b51e2fd681e24532c6393f391b
LenilsonAmorim <57912792+LenilsonAmorim@users.noreply.github.com>
2026-08-20 02:20:09 -0300

    Add files via upload

--- web/cliente/js/client.js
+++ web/cliente/js/client.js
@@ -68,3 +68,13 @@ $("trackBtn").onclick=async()=>{
 };
 load();
 
+
+\n// Busca principal do mobile
+document.addEventListener("DOMContentLoaded",()=>{
+  const s=document.getElementById("mobileSearch");
+  if(s) s.addEventListener("input",()=>{
+    const main=document.getElementById("search");
+    if(main){main.value=s.value; main.dispatchEvent(new Event("input"));}
+    else if(typeof renderProducts==="function") renderProducts();
+  });
+});
